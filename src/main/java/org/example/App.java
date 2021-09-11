b49ac24688f8b40e9bc34871247b02e0fcc9bea3
Curtiellas <89556000+Curtiellas@users.noreply.github.com>
2021-09-11 05:36:24 -0400

    Update App.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -24,10 +24,15 @@ public class App
         int num1 = Integer.parseInt(numString1);
         int num2 = Integer.parseInt(numString2);
 
+        int add = num1 + num2;
+        int sub = num1 - num2;
+        int mult = num1 * num2;
+        int div = num1 / num2;
+
         //entire output in one line of code, \n jumps to the next line
-        System.out.println( num1 + " + " + num2 + " = " + (num1 + num2) + "\n" +
-                            num1 + " - " + num2 + " = " + (num1 - num2) + "\n" +
-                            num1 + " * " + num2 + " = " + (num1 * num2) + "\n" +
-                            num1 + " / " + num2 + " = " + (num1 / num2) + "\n");
+        System.out.println( num1 + " + " + num2 + " = " + add + "\n" +
+                            num1 + " - " + num2 + " = " + sub + "\n" +
+                            num1 + " * " + num2 + " = " + mult + "\n" +
+                            num1 + " / " + num2 + " = " + div + "\n");
     }
 }
